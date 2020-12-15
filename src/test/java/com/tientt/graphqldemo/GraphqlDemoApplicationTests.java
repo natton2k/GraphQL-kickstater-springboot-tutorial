4d10f7b0868286f846d9244840493e6fa3310286
TienTruong98 <tientt1938@gmail.com>
2020-12-15 22:08:27 +0700

    Add SpringBoot entrypoint

---
+++ src/test/java/com/tientt/graphqldemo/GraphqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tientt.graphqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
