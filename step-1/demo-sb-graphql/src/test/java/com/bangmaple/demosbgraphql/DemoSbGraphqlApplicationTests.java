5dcc4c15cb21aa4831ab9fbb86104cbb8af22438
bangmaple <webcam1362000@gmail.com>
2020-12-16 16:44:15 +0700

    ✅ Completed the Step 1 tutorial and separated the steps project

---
+++ step-1/demo-sb-graphql/src/test/java/com/bangmaple/demosbgraphql/DemoSbGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bangmaple.demosbgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSbGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
